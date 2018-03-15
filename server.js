af8c330ee8d3930adfb7bbbceb775620c7145525
Larry Brouwer <larry.brouwer@tempora.com>
2018-03-15 13:17:25 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -19,9 +21,11 @@ app.use((req, res, next) => {
     });
     next();
 });
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -61,6 +65,7 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
